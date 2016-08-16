119e8667755df4eb3515b031fd2358bbef76d0f6
Valorad <wcxaaa@gmail.com>
2016-08-16 23:03:44 +0800

    login/logout Session!!! And nav,dragonborn adjustments.
    +Ajax log-in/log-out session! Finally!
    +Log-in box.
    *z-index is useless between elements in 2 different containers, as a
    result, nav-bar buttons under dragonborn image cannot be clicked. Now
    this changes to opacified nav-bar, to make compromise. Meanwhile,
    whenever dragonborn cannot be seen, the nav-bar color returns to normal
    opacity.

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "home"
 urlpatterns = [
 	#list
-	url(r'^$', views.home, name='HomePage')
+	url(r'^$', views.home, name='HomePage'),
+	url(r'^login/$', views.log_me_in, name='Login'),
+	url(r'^logout/$', views.log_me_out, name='Logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
-	a = 3
 	# return render_to_response("home/index.html", locals(), context_instance=RequestContext(request))
 	return render_to_response("home/index.html")
+
+def log_me_in(request):
+	name = request.POST.get('username')
+	pswd = request.POST.get('password')
+	user = authenticate(username=name, password=pswd)
+	userbkinfo = {"name" : name, "status" : ""}
+	if user is not None:
+		login(request, user)
+		userbkinfo["status"] = "success"
+		# return render_to_response("home/index.html")
+	else:
+		userbkinfo["status"] = "failure"
+
+	return JsonResponse(userbkinfo)
+
+def log_me_out(request):
+	logout(request)
+	return JsonResponse({"status" : True})
