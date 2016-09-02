8cebcc80a48fa4242a932a42c95a5c5cda0f84b1
Valorad <wcxaaa@gmail.com>
2016-09-02 12:50:29 +0800

    * fixed the issue#1
    IE全家桶 now login with no CSRF error.

--- home/static/Globalsite/js/wcNexusUtil.js
+++ home/static/Globalsite/js/wcNexusUtil.js
@@ -33,50 +33,50 @@
         );
     }
     /*  CSRF-config Start */
-$.ajaxSetup({ 
-     beforeSend: function(xhr, settings) {
-         function getCookie(name) {
-             var cookieValue = null;
-             if (document.cookie && document.cookie != '') {
-                 var cookies = document.cookie.split(';');
-                 for (var i = 0; i < cookies.length; i++) {
-                     var cookie = jQuery.trim(cookies[i]);
-                     // Does this cookie string begin with the name we want?
-                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                         break;
-                     }
-                 }
-             }
-             return cookieValue;
-         }
-         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-             // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-         }
-     } 
-});
-$(document).ajaxSend(function( event, xhr, settings ) {
-    if (settings.type == 'POST' || settings.type == 'PUT' || settings.type == 'DELETE') {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie != '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
-        }
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
-});
+// $.ajaxSetup({ 
+//      beforeSend: function(xhr, settings) {
+//          function getCookie(name) {
+//              var cookieValue = null;
+//              if (document.cookie && document.cookie != '') {
+//                  var cookies = document.cookie.split(';');
+//                  for (var i = 0; i < cookies.length; i++) {
+//                      var cookie = jQuery.trim(cookies[i]);
+//                      // Does this cookie string begin with the name we want?
+//                      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+//                          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                          break;
+//                      }
+//                  }
+//              }
+//              return cookieValue;
+//          }
+//          if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+//              // Only send the token to relative URLs i.e. locally.
+//              xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+//          }
+//      } 
+// });
+// $(document).ajaxSend(function( event, xhr, settings ) {
+//     if (settings.type == 'POST' || settings.type == 'PUT' || settings.type == 'DELETE') {
+//         function getCookie(name) {
+//             var cookieValue = null;
+//             if (document.cookie && document.cookie != '') {
+//                 var cookies = document.cookie.split(';');
+//                 for (var i = 0; i < cookies.length; i++) {
+//                     var cookie = jQuery.trim(cookies[i]);
+//                     // Does this cookie string begin with the name we want?
+//                     if (cookie.substring(0, name.length + 1) == (name + '=')) {
+//                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                         break;
+//                     }
+//                 }
+//             }
+//             return cookieValue;
+//         }
+//         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+//             // Only send the token to relative URLs i.e. locally.
+//             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+//         }
+//     }
+// });
     /*  CSRF-config End */    
