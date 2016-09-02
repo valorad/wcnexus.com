631d16f8a7b4842cd4c59191ae3d363bd39ec1d2
Valorad <wcxaaa@gmail.com>
2016-09-02 14:48:05 +0800

    create table SiteKV
    used to store (possibly) sensative info.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SiteKV(models.Model):
+	key = models.CharField(max_length=30)
+	value = models.CharField(max_length=50)
+	def __str__(self):
+		return self.key + " : " + self.value
