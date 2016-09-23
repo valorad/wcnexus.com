2ceda81f1c1c8d7bf0622f6d2183117b47c299f9
Valorad <wcxaaa@gmail.com>
2016-09-23 12:06:49 +0800

    + create venturer detail model
    and accessible by admin

--- venturer/models.py
+++ venturer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class venturerDetail(models.Model):
+	venturer = models.ForeignKey(User, on_delete=models.CASCADE)
+	alias = models.CharField(max_length=50, null=True)
+	shortBio = models.CharField(max_length=140, null=True)
+	def __str__(self):
+		if self.alias is None:
+			return "a venturer's detail"
+		if self.shortBio is None:
+			return self.alias + "'s detail"
+
+		return self.alias + ": " + self.shortBio
+
+	
