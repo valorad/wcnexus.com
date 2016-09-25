c65a9a8fb3ce66ded4e3d2f9ab4cd39d2f24eb10
Valorad <wcxaaa@gmail.com>
2016-09-25 15:18:25 +0800

    + use the help to handle articles
    first time ever use the help from Zinnia's Weblog to handle articles by
    specific venturer.

--- venturer/urls.py
+++ venturer/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+from zinnia.views.authors import AuthorDetail
+
 app_name = "venturer"
 urlpatterns = [
 	#list
 	# url(r'^$', views.venturerSelf, name='View_Own_Profile'),
 	url(r'^(?P<venturerName>[\w]+)/$', views.loadVenturer, name='View_a_Profile'),
 	url(r'^(?P<venturerName>[\w]+)/VCate/(?P<viewCate>[\w]+)$', views.loadBHeaderAjax, name='View_a_Profile_Specifing_Cate'),
+	url(r'^(?P<username>[.+-@\w]+)/VAuthor/$', AuthorDetail.as_view(), name='author_detail'),
 ]
