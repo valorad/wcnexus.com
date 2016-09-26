813c5d1686ee24cebc75f1488dbfdd9ace8bba7d
Valorad <wcxaaa@gmail.com>
2016-09-26 17:06:37 +0800

    * link column in SiteKV should be nullable

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class SiteKV(models.Model):
 	key = models.CharField(max_length=30)
 	value = models.CharField(max_length=50)
-	link = models.CharField(max_length=200, null=True)
+	link = models.CharField(max_length=200, null=True, blank=True)
 	def __str__(self):
 		return self.key + " : " + self.value
 
