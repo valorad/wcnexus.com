67600b859bb2cdbccf23fc35f8cea8064e852f74
Valorad <wcxaaa@gmail.com>
2016-09-26 17:13:00 +0800

    + url pattern for tag Ajax load

--- venturer/urls.py
+++ venturer/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	#list
 	# url(r'^$', views.venturerSelf, name='View_Own_Profile'),
 	url(r'^(?P<venturerName>[\w]+)/$', views.loadVenturer, name='View_a_Profile'),
+	url(r'^(?P<venturerName>[\w]+)/VTag/$', views.loadBTagsAjax, name='View_a_Profile_Specifing_Tag'),
 	url(r'^(?P<venturerName>[\w]+)/VCate/(?P<viewCate>[\w]+)$', views.loadBHeaderAjax, name='View_a_Profile_Specifing_Cate'),
 	url(r'^(?P<username>[.+-@\w]+)/VAuthor/$', AuthorDetail.as_view(), name='author_detail'),
 ]
