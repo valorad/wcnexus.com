56a62b51a431aba806e17c9266a081ea2a550ad6
Valorad <wcxaaa@gmail.com>
2017-02-04 18:19:22 +0800

    - changing to v2
    Boom!~

--- home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import SiteKV, SiteHot
-
-# Register your models here.
-admin.site.register(SiteKV)
-admin.site.register(SiteHot)
--- venturer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(venturerDetail)
-admin.site.register(vBlogCategory)
-admin.site.register(vBlogTags)
