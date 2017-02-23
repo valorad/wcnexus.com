b377400fdf87f11304c3b1f95531135a8da6ade2
Valorad <wcxaaa@gmail.com>
2017-02-23 15:13:25 +0800

    * test of changing themes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
+
+  themes = [
+    'white',
+    'black'
+  ];
+
+  currentClass: string = this.themes[0];
+  baseClass = " display col-md-12 card";
+  currentAppliedClass = this.currentClass + this.baseClass;
+
+  changeClass(toClass: string) {
+    this.currentClass = toClass;
+    this.currentAppliedClass = toClass + this.baseClass;
+  }
+
+
 }
