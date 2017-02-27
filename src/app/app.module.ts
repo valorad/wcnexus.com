c03e6fa368b239fc6128712bb0892df44e8b7fd4
Valorad <wcxaaa@gmail.com>
2017-02-27 15:30:29 +0800

    + add Angular route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { wcnexusRoutes } from './wcnexus.route';
+
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { Http404Component } from './http404/http404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    Http404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    wcnexusRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
