b01ad4acb1528b736a9f35f715643148db76593a
Valorad <wcxaaa@gmail.com>
2017-02-28 11:31:46 +0800

    * (WIP) immersive navbar_2  * handled nav content and sign in/up component in screen less than lg.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,8 +25,23 @@ export class AppComponent {
   //nav bar class
   private navbarClass: string = "Opacified";
 
+  private navbarMenuClass: string = "navbar-nav mr-auto HiddenltLG";
+
+
   toggleNav() {
     this.navbarClass = (this.navbarClass == "" ? "Opacified" : "");
+  }
+
+  toggleNavMenu() {
+    let startPos = this.navbarMenuClass.indexOf("HiddenltLG");
+    // if navbarMenuClass contains "HiddenltLG" then remove this class
+    if (startPos >= 0) {
+      this.navbarMenuClass = this.navbarMenuClass.slice(0, startPos - 1);
+    }
+    else {
+      // navbarMenuClass does not contain "HiddenltLG", then add this class
+      this.navbarMenuClass += " HiddenltLG";
+    }
   }
 
 
