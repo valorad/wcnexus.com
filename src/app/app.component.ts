34d3dab8a93a858c55106c3b1ebcc8fd1c7eeaef
Valorad <wcxaaa@gmail.com>
2017-02-28 15:19:29 +0800

    * title adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'wcNexus';
 
   themes = [
     'white',
