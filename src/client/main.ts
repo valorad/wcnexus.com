68ab1a622127b57e05f97d7861070245331043a4
valorad <valorad@outlook.com>
2017-12-13 17:11:55 +0800

    + enables WDS HMR

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,4 +4,11 @@ import { AppModule } from './app/app.module';
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
+
+// Enables WDS Hot Module Replacement.
+declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
