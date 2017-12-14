1e88913f68b973af1fe57aede83a2c3a9972d803
valorad <valorad@outlook.com>
2017-12-14 16:33:01 +0800

    + designed site intro part

--- src/client/app/index/index.component.ts
+++ src/client/app/index/index.component.ts
@@ -10,7 +10,24 @@ import { ThemeService } from '../_services/theme.service';
 })
 export class IndexComponent implements OnInit {
 
+	photoNum = 30;
+
 	dummyAvt: any[] = [];
+	dummyTechs: any[] = [];
+
+	fetchphoto: (num: string) => {
+		// TO DO...
+
+		// fetch photo from backend
+		// e.g. GET /api/photo/0
+		// if backend has only 3 photos
+		// 1 done, 2 done, 3 done, 4 oh no, get photo 1 again.
+		// if backend has no single photo
+		// 1 oh no, backend tells us no there is nothing there,
+		// so we stop asking backend, and have to just replace every single
+		// one with fallback img stored at front.
+
+	};
 
 	constructor(
 		public themeService: ThemeService
@@ -18,12 +35,25 @@ export class IndexComponent implements OnInit {
 	
 	ngOnInit() {
 		this.themeService.config.current = "mat-blue-pink";
-		for (let i = 0; i < 30; i++) {
+
+		// fill photowall with dummy
+		for (let i = 0; i < this.photoNum; i++) {
 			this.dummyAvt.push({
 				title: i,
 				img: null
 			})
 		}
+
+		// dummy tech landfill
+		for (let i = 0; i < 15; i++) {
+			this.dummyTechs.push({
+				title: "A Tech no lo gy",
+				descr: "This is an amazing tech. Don't worry, this will be a formal description.",
+				img: null,
+				link: "https://www.link-to-the-dummy-tech.org.tv"
+			})
+		}
+
 	}
 
 }
